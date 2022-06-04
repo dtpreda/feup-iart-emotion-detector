1c35a9040c28e67e3109b9e1bc0875d1f7c50bf6
Bruno Mendes <bd_mendes@outlook.com>
2022-06-04 18:43:20 +0100

    Add neural network

--- emotion_detector/src/classifiers/generative.py
+++ emotion_detector/src/classifiers/generative.py
@@ -1,7 +1,15 @@
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 
 
 def random_forest_predict(x_train, y_train, x_test):
     classifier = RandomForestClassifier(n_estimators=200, random_state=0)
+    classifier.fit(x_train, y_train)
+    return classifier.predict(x_test)
+
+
+def multi_layer_perceptron_predict(x_train, y_train, x_test):
+    classifier = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                               hidden_layer_sizes=(5, 2), random_state=1)
     classifier.fit(x_train, y_train)
     return classifier.predict(x_test)
--- emotion_detector/src/main.py
+++ emotion_detector/src/main.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from classifiers.generative import random_forest_predict
+from classifiers.generative import random_forest_predict, multi_layer_perceptron_predict
 from classifiers.discriminative import gaussian_naive_bayes_predict, multinomial_naive_bayes_predict
 
 from text_analysis.feature_extraction import tfidf_matrix
@@ -21,7 +21,7 @@ processed_statements = tfidf_matrix(statements)
 x_train, x_test, y_train, y_test = train_test_split(
     processed_statements, emotions, test_size=0.2, random_state=0)
 
-train_predictions = random_forest_predict(
+train_predictions = multi_layer_perceptron_predict(
     x_train, y_train, x_test)
 
 print(
@@ -34,7 +34,7 @@ test_statements = [tokenize(statement[0])
                    for statement in test_statements]
 processed_test_statements = tfidf_matrix(test_statements)
 
-test_predictions = random_forest_predict(
+test_predictions = multi_layer_perceptron_predict(
     x_train, y_train, processed_test_statements)
 
 print(
