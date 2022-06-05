8a22506db6831a5f9b297eb93aa3ca22128b7aa9
Fernando Rego <fernandoluisrego@gmail.com>
2022-06-05 22:38:17 +0100

    Add submit button

--- emotion_detector/src/controller/pygame_controller.py
+++ emotion_detector/src/controller/pygame_controller.py
@@ -1,6 +1,8 @@
 import pygame
+import pygame_widgets
 from pygame_texteditor import TextEditor
 from time import time, sleep
+from pygame_widgets.button import Button
 
 from view.main_view import MainView
 
@@ -9,10 +11,15 @@ FPS = 30
 
 # Text Area Constants
 STYLE_FILE = "emotion_detector/asset/style/editor.yml"
+FONT_SIZE = 21
 OFFSET_X = 50
 OFFSET_Y = 300
 TEXTAREA_WIDTH = 600
 TEXTAREA_HEIGHT = 300
+
+# Buttons
+COLOR_BUTTON = (190, 190, 190)
+HOVER_BUTTON = (150, 150, 150)
 
 
 class PygameController:
@@ -21,6 +28,7 @@ class PygameController:
         self.window = self.init_pygame()
         self.textarea = self.init_textarea()
         self.main_view = MainView(self.window, self.textarea, self.window_size)
+        self.submit_button = self.init_submit_button()
 
     def pygame_loop(self) -> None:
         self.window = self.init_pygame()
@@ -34,35 +42,13 @@ class PygameController:
                 break
 
             self.main_view.draw(events)
-
+            pygame_widgets.update(events)
             pygame.display.flip()
 
             sleep(max(1.0/FPS - (time() - start), 0))
             continue
 
         self.quit_pygame()
-
-    def init_pygame(self) -> None:
-        """
-        Pygame and screen initialization 
-
-        Return:
-            Pygame Surface to display
-        """
-        pygame.init()
-        return pygame.display.set_mode(self.window_size)
-
-    def init_textarea(self):
-        textarea = TextEditor(
-            OFFSET_X, OFFSET_Y, TEXTAREA_WIDTH, TEXTAREA_HEIGHT, self.window)
-        textarea.set_font_size(20)
-        textarea.set_syntax_highlighting(True)
-        textarea.set_colorscheme_from_yaml(STYLE_FILE)
-        return textarea
-
-    def quit_pygame(self) -> None:
-        '''Unitialize all pygame modules'''
-        pygame.quit()
 
     def is_running(self, events) -> bool:
         """
@@ -75,3 +61,38 @@ class PygameController:
             if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
                 return False
         return True
+
+    def init_pygame(self) -> None:
+        """
+        Pygame and screen initialization 
+
+        Return:
+            Pygame Surface to display
+        """
+        pygame.init()
+        return pygame.display.set_mode(self.window_size)
+
+    def quit_pygame(self) -> None:
+        '''Unitialize all pygame modules'''
+        pygame.quit()
+
+    def init_textarea(self):
+        textarea = TextEditor(
+            OFFSET_X, OFFSET_Y, TEXTAREA_WIDTH, TEXTAREA_HEIGHT, self.window)
+        textarea.set_font_size(FONT_SIZE)
+        textarea.set_syntax_highlighting(True)
+        textarea.set_colorscheme_from_yaml(STYLE_FILE)
+        return textarea
+
+    def init_submit_button(self):
+        return Button(
+            self.window, 100, 650, 150, 50,
+            text='SUBMIT',
+            fontSize=35,
+            margin=20,
+            inactiveColour=COLOR_BUTTON,
+            hoverColour=HOVER_BUTTON,
+            pressedColour=HOVER_BUTTON,
+            radius=20,
+            onClick=lambda: print('Click')
+        )
