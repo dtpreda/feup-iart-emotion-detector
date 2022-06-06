aa7a1c8d0e2f4659d58d4e52789e01bf25dd3bad
bdmendes <bd_mendes@outlook.com>
2022-06-06 18:12:23 +0100

    Change model api

--- emotion_detector/src/classifiers/generative.py
+++ emotion_detector/src/classifiers/generative.py
@@ -2,14 +2,22 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.neural_network import MLPClassifier
 
 
-def random_forest_predict(x_train, y_train, x_test):
-    classifier = RandomForestClassifier(n_estimators=200, random_state=0)
-    classifier.fit(x_train, y_train)
-    return classifier.predict(x_test)
+random_classifier = RandomForestClassifier(n_estimators=200, random_state=0)
+perceptron_classifier = MLPClassifier(solver='lbfgs', alpha=1e-5,
+                                      hidden_layer_sizes=(5, 2), random_state=0)
 
 
-def multi_layer_perceptron_predict(x_train, y_train, x_test):
-    classifier = MLPClassifier(solver='lbfgs', alpha=1e-5,
-                               hidden_layer_sizes=(5, 2), random_state=1)
-    classifier.fit(x_train, y_train)
-    return classifier.predict(x_test)
+def random_forest_fit(x_train, y_train):
+    random_classifier.fit(x_train, y_train)
+
+
+def random_forest_predict(x_test):
+    return random_classifier.predict(x_test)
+
+
+def multi_layer_perceptron_fit(x_train, y_train):
+    perceptron_classifier.fit(x_train, y_train)
+
+
+def multi_layer_perceptron_predict(x_test):
+    return perceptron_classifier.predict(x_test)
--- emotion_detector/src/predictions/datasets_prediction.py
+++ emotion_detector/src/predictions/datasets_prediction.py
@@ -2,10 +2,7 @@ import pandas as pd
 from text_analysis.preprocess import tokenize
 from text_analysis.feature_extraction import tfidf_learn_vocabulary, tfidf_matrix
 from sklearn.metrics import accuracy_score
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from classifiers.generative import random_forest_predict, multi_layer_perceptron_predict
-from classifiers.discriminative import gaussian_naive_bayes_predict, multinomial_naive_bayes_predict
 
 
 def predict_dataset(algorithm, dataset_dir, rm_stop_words,
@@ -25,11 +22,7 @@ def predict_dataset(algorithm, dataset_dir, rm_stop_words,
     x_train, x_test, y_train, y_test = train_test_split(
         processed_statements, emotions, test_size=0.2, random_state=0)
 
-    train_predictions = algorithm(
-        x_train, y_train, x_test)
-
-    # print(
-    #    f"Accuracy on 20% of the train dataset: {accuracy_score(y_test, train_predictions)}")
+    train_predictions = algorithm(x_train, y_train, x_test)
 
     test_emotions = test_data.Emotion.values
     test_statements = test_data.drop('Emotion', axis=1).values
@@ -41,8 +34,5 @@ def predict_dataset(algorithm, dataset_dir, rm_stop_words,
 
     test_predictions = algorithm(
         x_train, y_train, processed_test_statements)
-
-    # print(
-    #    f"Accuracy on the test dataset: {accuracy_score(test_emotions, test_predictions)}")
 
     return (accuracy_score(y_test, train_predictions), accuracy_score(test_emotions, test_predictions))
